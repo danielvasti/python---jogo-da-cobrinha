7659f4f0a74f9dce4de882886533b4fde98446f7
Daniel Vasti <danielvasticr@gmail.com>
2025-01-29 10:06:31 -0300

    correcao de bug

--- snakegame.py
+++ snakegame.py
@@ -3,48 +3,68 @@ import curses
 def game_loop(window):
     #setup inicial
     curses.curs_set(0)
-    actor = [10, 15]
-    current_direction = curses.KEY_RIGHT
+    snake = [[10, 15],
+             [9, 15],
+             [8, 15],
+             [7, 15]]
+    current_direction = curses.KEY_DOWN
     
     while True:
         draw_screen(window=window)
-        draw_actor(actor=actor, window=window)
+        draw_snake(snake=snake, window=window)
         direction = get_new_direction(window=window, timeout=1000)
-        if direction is not None:
+        if direction is None:
             direction = current_direction
-        move_actor(actor=actor, direction=direction)
-        if actor_hit_border(actor=actor, window=window):
+        move_snake(snake=snake, direction=direction)
+        if snake_hit_border(snake=snake, window=window):
             return
         current_direction = direction
         
+def get_new_direction(window, timeout):
+    window.timeout(timeout)
+    direction = window.getch()
+    if direction in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT]:
+        return direction
+    return None
+
+def snake_hit_border(snake, window):
+    head = snake[0]
+    return actor_hit_border(actor=head, window=window)
+
 def draw_screen(window):
     window.clear()  # Clear the screen first
     window.border(0)  # Then draw the border
         
-def draw_actor(actor, window):
-    window.addch(actor[0], actor[1], curses.ACS_DIAMOND)
+def draw_snake(snake, window):
+    head = snake[0]
+    draw_actor(actor=head, window=window, char='@')
+    body = snake[1:]
+    for body_part in body:
+        draw_actor(actor=body_part, window=window, char='s')
 
-def get_new_direction(window, timeout):
-        window.timeout(timeout)
-        direction = window.getch()
-        if direction in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT]:
-            return direction
-        return None
+def draw_actor(actor, window, char):
+    window.addch(actor[0], actor[1], char)
+
+def move_snake(snake, direction):
+    head = snake[0].copy()
+    move_actor(actor=head, direction=direction)
+    snake.insert(0, head)
+    snake.pop()
 
 def move_actor(actor, direction):
-        match direction:
-            case curses.KEY_UP:
-                actor[0] -= 1
-            case curses.KEY_DOWN:    
-                actor[0] += 1
-            case curses.KEY_LEFT:  
-                actor[1] -= 1
-            case curses.KEY_RIGHT:
-                actor[1] += 1
-            case curses.KEY_F1: # pessoa apertou F1 para sair
-                exit()  # Exit the game
-            case _: # pessoa apertou qualquer outra tecla
-                pass 
+    match direction:
+        case curses.KEY_UP:
+            actor[0] -= 1
+        case curses.KEY_DOWN:    
+            actor[0] += 1
+        case curses.KEY_LEFT:  
+            actor[1] -= 1
+        case curses.KEY_RIGHT:
+            actor[1] += 1
+        case curses.KEY_F1: # pessoa apertou F1 para sair
+            exit()  # Exit the game
+        case _: # pessoa apertou qualquer outra tecla
+            pass 
              
 def actor_hit_border(actor, window):
     height, width = window.getmaxyx()
