1ecaff02607077641835cbdc33aaa7f1c20bbe20
Daniel Vasti <danielvasticr@gmail.com>
2025-01-29 09:42:16 -0300

    1

--- snakegame.py
+++ snakegame.py
@@ -4,15 +4,18 @@ def game_loop(window):
     #setup inicial
     curses.curs_set(0)
     actor = [10, 15]
+    current_direction = curses.KEY_RIGHT
     
     while True:
         draw_screen(window=window)
         draw_actor(actor=actor, window=window)
         direction = get_new_direction(window=window, timeout=1000)
         if direction is not None:
-            move_actor(actor=actor, direction=direction)
+            direction = current_direction
+        move_actor(actor=actor, direction=direction)
         if actor_hit_border(actor=actor, window=window):
             return
+        current_direction = direction
         
 def draw_screen(window):
     window.clear()  # Clear the screen first
